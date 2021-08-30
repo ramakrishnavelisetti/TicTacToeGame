c06c085cf1afda0e9b779ca5de4e0d3004e36aa5
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-08-30 09:35:48 +0530

    Added: Create Board

--- src/main/java/com/bdlz/TTT/TicTacToe.java
+++ src/main/java/com/bdlz/TTT/TicTacToe.java
@@ -1,7 +1,15 @@
 package com.bdlz.TTT;
 
 public class TicTacToe {
+    public static void createBoard() {
+        char[] board = new char[10];
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe Game");
+        createBoard();
     }
 }
