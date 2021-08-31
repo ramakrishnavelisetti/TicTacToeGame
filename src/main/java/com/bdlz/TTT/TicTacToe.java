e86637bffaf9c2080344eb2d7ce5b6a799d32155
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-08-31 14:15:50 +0530

    Added: Show board

--- src/main/java/com/bdlz/TTT/TicTacToe.java
+++ src/main/java/com/bdlz/TTT/TicTacToe.java
@@ -4,11 +4,29 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static Scanner sc = new Scanner(System.in);
+    public static char[] board = new char[10];
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic-Tac-Toe Game");
+        createBoard();
+        chooseChoice();
+        showBoard();
+    }
+
     public static void createBoard() {
-        char[] board = new char[10];
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
+    }
+    public static void showBoard() {
+            System.out.println();
+            System.out.println("     |     |     ");
+            System.out.println("   " + board[1] + " |   " + board[2] + " | " + board[3] + " ");
+            System.out.println("_____|_____|_____");
+            System.out.println("   " + board[4] + " |   " + board[5] + " | " + board[6] + " ");
+            System.out.println("_____|_____|_____");
+            System.out.println("   " + board[7] + " |   " + board[8] + " | " + board[9] + " ");
+            System.out.println("     |     |     ");
     }
     static void chooseChoice() {
         System.out.println("Choose x or 0");
@@ -24,23 +42,5 @@ public class TicTacToe {
         }
 
     }
-    public static void showBoard() {
-        int count = 1;
-        int board[][] = new int[3][3];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                board[i][j] = count++;
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
 
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tic-Tac-Toe Game");
-        createBoard();
-        chooseChoice();
-        showBoard();
-    }
 }
